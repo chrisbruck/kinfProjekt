a74443ce63a02cacb1865f4271d11ffb10a0a111
itat86 <tatjana-schindler@gmx.de>
2015-05-03 23:09:40 +0200

    Version ReadCSV von Tati

---
+++ src/readCSV/ReadCSVFileTati.java
@@ -0,0 +1,41 @@
+package readCSV;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadCSVFileTati {
+	public void readFile(String csvFileToRead) {
+		BufferedReader br = null;
+		String line = "";
+		String splitBy = ";";
+		try {
+			br = new BufferedReader(new FileReader(csvFileToRead));
+			while ((line = br.readLine()) != null) {
+				String[] products = line.split(splitBy);
+				System.out.println("Products [Code= " + products[0]
+						+ " , Desc=" + products[1] + " , Quantity="
+						+ products[2] + "]");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]) {
+		String csvFileToRead = "C:/Users/Tatjana/Desktop/test.csv";
+		ReadCSVFileTati reader = new ReadCSVFileTati();
+		reader.readFile(csvFileToRead);
+	}
+}
