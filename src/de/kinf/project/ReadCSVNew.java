b988ac73a244b1465da802fc20d7151d0f9f3e7d
chris <christoph.bartmann@gmx.de>
2015-05-26 12:11:24 +0200

    implementing a more generic CSVReader

--- src/de/kinf/project/ReadCSVNew.java
+++ src/de/kinf/project/ReadCSVNew.java
@@ -22,12 +22,14 @@ import com.opencsv.CSVReader;
 public class ReadCSVNew {
 
 	Connection conn;
-	
-	//HashMap for storing the column names
-	
-	Map<Integer, Integer > columnsMap = new HashMap<>();
-	Map<String, Integer > firstNameMap = new HashMap<>();
+
+	// HashMap for storing the column names
+	private Map<Integer, Integer> columnsMap = new HashMap<>();
+	// HashMap for storing the column names
+	private Map<String, Integer> columnsNames = new HashMap<>();
+	private Map<String, Integer> firstNameMap = new HashMap<>();
 	int firstNameCounter = 0;
+	private static final int VORNAME_CAT = 0;
 
 	public static void main(String[] args) throws ParseException,
 			ClassNotFoundException, SQLException {
@@ -46,68 +48,34 @@ public class ReadCSVNew {
 
 	public void run() throws SQLException {
 
-				
 		// String csvFile =
 		// "nik-2:/ christian$/Applications/XAMPP/xamppfiles/htdocs/miproject/migrationstreams/build/importCSV/data.csv";
-		//URL url = Resource("example_data.csv");
-		//String csvFile = url.getPath().toString();
+		// URL url = Resource("example_data.csv");
+		// String csvFile = url.getPath().toString();
 
 		BufferedReader br = null;
 		String[] splittedRow;
 		String csvSplitBy = ",";
 		int counter = 0;
 		try {
-  		    InputStream is = ReadCSVNew.class.getResourceAsStream("\\example_data.csv");
-		   	br = new BufferedReader(new InputStreamReader(is, "UTF8"));
-		   	CSVReader csvr = new CSVReader(br);
-		   	String[] splittedColumns = csvr.readNext();
-		 					
-		   	//reading column titles to map, used for mapping to primary keys 
-			for (int i= 0; i< splittedColumns.length; i++){
-				System.out.println(i + " " + splittedColumns[i]);
-				if (splittedColumns[i].contains("HS B")){
-					columnsMap.put(i, 1);
-				}
-				else if (splittedColumns[i].contains("HS C")){
-					columnsMap.put(i, 2);
-				}
-				
-				else if (splittedColumns[i].contains("HS D")){
-					columnsMap.put(i, 3);
-				}
-				else if (splittedColumns[i].contains("HS E")){
-					columnsMap.put(i, 4);
-				}
-				else if (splittedColumns[i].contains("HS F")){
-					columnsMap.put(i, 5);
-				}
-				else if (splittedColumns[i].contains("HS G")){
-					columnsMap.put(i, 6);
-				}
-				else if (splittedColumns[i].contains("HS H")){
-					columnsMap.put(i, 7);
-				}
-				else if (splittedColumns[i].contains("HS I")){
-					columnsMap.put(i, 8);
-				}
-				else if (splittedColumns[i].contains("HS J")){
-					columnsMap.put(i, 9);
-				}
-				else if (splittedColumns[i].contains("V E 38")){
-					columnsMap.put(i, 10);
-				}
-			}
-				
-				
-				
+			InputStream is = ReadCSVNew.class
+					.getResourceAsStream("\\example_data.csv");
+			br = new BufferedReader(new InputStreamReader(is, "UTF8"));
+			CSVReader csvr = new CSVReader(br);
+			String[] splittedColumns = csvr.readNext();
+
+			generateColumnsMap(splittedColumns);
+			generateConstantValues(splittedColumns);
+
 			System.out.println(columnsMap);
+			System.out.println(columnsNames);
 			/*
 			 * int personId = 0; int countryOfBirthId; int denominationId; int
 			 * professionalCategoryId;
 			 */
-		
+
 			while ((splittedRow = csvr.readNext()) != null) {
-				//System.out.println(line);
+				// System.out.println(line);
 				counter++;
 
 				int pid = Integer.parseInt(splittedRow[0]);
@@ -115,38 +83,28 @@ public class ReadCSVNew {
 				String nummer_hess = (splittedRow[2]);
 				String anrede = splittedRow[3];
 				String anrede_normal = splittedRow[4];
-								
-				//firstName Stuff
-				int firstNameKey;
+
+				// firstName Stuff
+				int nameKey;
 				String vorname = splittedRow[5];
-				//firstNameKey = writeFirstName(vorname, columnsMap.get(7), pid);
+				// firstNameKey = writeFirstName(vorname, columnsMap.get(7),
+				// pid);
 				String vornameNormal = splittedRow[6];
-				//firstNameKey = writeFirstName(vornameNormal, columnsMap.get(9), pid);
-				for (int j = 7; j <=16; j++){
-					if (!splittedRow[j].equals("")){
-						//System.out.println("check" + " " + counter +  " " + j);
-						firstNameKey = writeFirstName(splittedRow[j], columnsMap.get(j), pid);
+				// firstNameKey = writeFirstName(vornameNormal,
+				// columnsMap.get(9), pid);
+				for (int j = columnsNames.get("VORNAME HS B (AUB, I 11)"); j <= columnsNames
+						.get("VORNAME AUB, V E 38)"); j++) {
+					if (!splittedRow[j].equals("")) {
+						nameKey = getKeyFromDatabase();
+						int[] values = { pid, nameKey, columnsMap.get(j),
+								VORNAME_CAT };
+						writetoConnectingTable(values);
 					}
 				}
-				
-				//lastName Stuff
-				
-				
-				
-//			
-//				String insertPerson = "INSERT INTO personTest(pid, seite_original, nummer_hess)"
-//						+ " VALUES (?, ?, ?)";
-//				PreparedStatement prepareInsertPerson = conn
-//						.prepareStatement(insertPerson);
-//				prepareInsertPerson.setInt(1, pid);
-//				prepareInsertPerson.setInt(2, seite_original);
-//				prepareInsertPerson.setString(3, nummer_hess);
 
-				
-				 }
-			//System.out.println(firstNameMap);
-			
-			
+				// lastName Stuff
+
+			}
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -165,38 +123,101 @@ public class ReadCSVNew {
 		}
 
 	}
-	
-	private int writeFirstName(String firstName, int sourceId, int personId){
-		int firstNameKey = 0;
-		if (!firstNameMap.containsKey(firstName)){
-			firstNameCounter++;
-			firstNameMap.put(firstName, firstNameCounter);
-			firstNameKey = firstNameCounter;
-			
-		}else{
-			firstNameKey = firstNameMap.get(firstName);
+
+	private void writetoConnectingTable(int[] values) {
+		PreparedStatement preparedStatement;
+		String insertString="";
+		switch (values[3]) {
+		case VORNAME_CAT:
+			insertString = "INSERT INTO personen_vornamen(personen_id, vornamen_id,quellen_id) "
+					+ " VALUES (?, ?, ?)";
+			break;
+
+		default:
+			break;
 		}
-		String insertFirstName = "INSERT INTO personen_vornamen(personen_id, vornamen_id,quellen_id) "
-				 + " VALUES (?, ?, ?)";
-		System.out.println(personId + " " + firstName + " " + firstNameKey +" "+ sourceId);
-		PreparedStatement prepareInsertFirstName;
 		try {
-			prepareInsertFirstName = conn
-					.prepareStatement(insertFirstName);
-			prepareInsertFirstName.setInt(1, personId);
-			prepareInsertFirstName.setInt(2, firstNameKey);
-			prepareInsertFirstName.setInt(3, sourceId);
-			//prepareInsertFirstName.executeUpdate();
+			preparedStatement = conn.prepareStatement(insertString);
+			preparedStatement.setInt(1, values[0]);
+			preparedStatement.setInt(2, values[1]);
+			preparedStatement.setInt(3, values[2]);
+			// prepareInsertFirstName.executeUpdate();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	
+
+	}
+
+	private int getKeyFromDatabase() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	private void generateConstantValues(String[] splittedColumns) {
+		for (int i = 0; i < splittedColumns.length; i++) {
+			columnsNames.put(splittedColumns[i], i);
+		}
+	}
+
+	private void generateColumnsMap(String splittedColumns[]) {
+		// reading column titles to map, used for mapping to primary keys
+		// hier vielleicht auch noch Kurzfassung wie VN_HS_B unterbringen
+		for (int i = 0; i < splittedColumns.length; i++) {
+			System.out.println(i + " " + splittedColumns[i]);
+			if (splittedColumns[i].contains("HS B")) {
+				columnsMap.put(i, SourcesList.HS_B);
+			} else if (splittedColumns[i].contains("HS C")) {
+				columnsMap.put(i, SourcesList.HS_C);
+			}
+
+			else if (splittedColumns[i].contains("HS D")) {
+				columnsMap.put(i, SourcesList.HS_D);
+			} else if (splittedColumns[i].contains("HS E")) {
+				columnsMap.put(i, SourcesList.HS_E);
+			} else if (splittedColumns[i].contains("HS F")) {
+				columnsMap.put(i, SourcesList.HS_F);
+			} else if (splittedColumns[i].contains("HS G")) {
+				columnsMap.put(i, SourcesList.HS_G);
+			} else if (splittedColumns[i].contains("HS H")) {
+				columnsMap.put(i, SourcesList.HS_H);
+			} else if (splittedColumns[i].contains("HS I")) {
+				columnsMap.put(i, SourcesList.HS_I);
+			} else if (splittedColumns[i].contains("HS J")) {
+				columnsMap.put(i, SourcesList.HS_J);
+			} else if (splittedColumns[i].contains("V E 38")) {
+				columnsMap.put(i, SourcesList.AUB_V);
+			}
+		}
+	}
+
+	private int writeFirstName(String firstName, int sourceId, int personId) {
+		int firstNameKey = 0;
+		if (!firstNameMap.containsKey(firstName)) {
+			firstNameCounter++;
+			firstNameMap.put(firstName, firstNameCounter);
+			firstNameKey = firstNameCounter;
+
+		} else {
+			firstNameKey = firstNameMap.get(firstName);
+		}
+		String insertFirstName = "INSERT INTO personen_vornamen(personen_id, vornamen_id,quellen_id) "
+				+ " VALUES (?, ?, ?)";
+		System.out.println(personId + " " + firstName + " " + firstNameKey
+				+ " " + sourceId);
+		PreparedStatement prepareInsertFirstName;
+		try {
+			prepareInsertFirstName = conn.prepareStatement(insertFirstName);
+			prepareInsertFirstName.setInt(1, personId);
+			prepareInsertFirstName.setInt(2, firstNameKey);
+			prepareInsertFirstName.setInt(3, sourceId);
+			// prepareInsertFirstName.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		return firstNameKey;
 	}
-	
 
-	
 }
---
+++ src/de/kinf/project/SourcesList.java
@@ -0,0 +1,17 @@
+package de.kinf.project;
+
+public class SourcesList {
+	
+	public static final int HS_B=  1;
+	public static final int HS_C=  2;
+	public static final int HS_D=  3;
+	public static final int HS_E=  4;
+	public static final int HS_F=  5;
+	public static final int HS_G=  6;
+	public static final int HS_H=  7;
+	public static final int HS_I=  8;
+	public static final int HS_J=  9;
+	public static final int AUB_V=  10;
+
+}
+
